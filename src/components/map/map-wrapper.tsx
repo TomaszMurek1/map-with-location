7ecfa7ff549936fa5c79eb70d1e3160c02d15aee
Tomasz Muraszko <tomasz.muraszko@wp.pl>
2022-08-28 20:10:19 +0200

    refactoring

--- src/components/map/map-wrapper.tsx
+++ src/components/map/map-wrapper.tsx
@@ -1,5 +1,8 @@
-import React from "react"
-import Map from './map'
-export const MapWrapper:React.FC<{ children: React.ReactNode, id:any }> =({children, id}) =>{
-    return <Map id={id}/>
-}
+import React from "react";
+import Map from "./map";
+export const MapWrapper: React.FC<{ children: React.ReactNode; id: any }> = ({
+  children,
+  id,
+}) => {
+  return <Map id={id} />;
+};
